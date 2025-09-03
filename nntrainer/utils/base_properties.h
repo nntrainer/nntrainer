f25c6cfa6dca32896ff226fdec5486ab40edd410
Hyeonseok Lee <hs89.lee@samsung.com>
2025-09-03 16:24:54 +0900

    [layer] enable fp16 dtype for input tensor
     - Enable fp16 data type for input tensor of input layer
     - Added InputTensorDataType class to specify model input type of input layer
     - The content described above applies to the input layer only.
    
    Signed-off-by: Hyeonseok Lee <hs89.lee@samsung.com>

--- nntrainer/utils/base_properties.h
+++ nntrainer/utils/base_properties.h
@@ -706,6 +706,41 @@ struct TensorTypeInfo {
 namespace props {
 
 /**
+ * @brief Input Data Type Enumeration Information
+ *  This property is differentiated with TensorDataType in that it doesn't have
+ *  default value
+ */
+class InputTensorDataType final : public EnumProperty<TensorDataTypeInfo> {
+public:
+  using prop_tag = enum_class_prop_tag;
+  static constexpr const char *key = "input_dtype";
+};
+
+/**
+ * @brief Weight Data Type Enumeration Information
+ *  This property can be used when any layer is created.
+ *  This property is differentiated with TensorDataType in that it doesn't have
+ *  default value
+ */
+class WeightDtype final : public EnumProperty<TensorDataTypeInfo> {
+public:
+  using prop_tag = enum_class_prop_tag;
+  static constexpr const char *key = "weight_dtype";
+
+  /**
+   * @brief Constructor
+   */
+  WeightDtype(){};
+
+  /**
+   * @brief Constructor
+   *
+   * @param value value to set
+   */
+  WeightDtype(TensorDataTypeInfo::Enum value) { set(value); };
+};
+
+/**
  * @brief Activation Enumeration Information
  *
  */
