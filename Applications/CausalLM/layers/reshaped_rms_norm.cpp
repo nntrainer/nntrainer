0783f32312be3d2409389b13471fc4c2f5f67637
Jungwon-Lee <jungone.lee@samsung.com>
2026-05-17 01:03:28 +0900

    [causallm] Use RMSNorm width kernels in reshaped RMSNorm
    Route FP32 and FP16 reshaped RMSNorm inputs through the dedicated width RMSNorm kernels.
    
    Signed-off-by: Jungwon-Lee <jungone.lee@samsung.com>

--- Applications/CausalLM/layers/reshaped_rms_norm.cpp
+++ Applications/CausalLM/layers/reshaped_rms_norm.cpp
@@ -84,14 +84,16 @@ void ReshapedRMSNormLayer::incremental_forwarding(
     if (in_step.getDataType() == ml::train::TensorDim::DataType::FP32) {
       ///@todo rms_norm_wrt_width_something() should be refactored to
       /// nntrainer::Tensor operation.
-#ifdef ENABLE_FP16
-      nntrainer::rms_norm_wrt_width_fp16_intrinsic(
-        in_step.getData<float>(), out_step.getData<float>(),
-        in_step.getDim().height(), in_step.getDim().width(), epsilon);
-#else
       nntrainer::rms_norm_wrt_width_fp32_intrinsic(
         in_step.getData<float>(), out_step.getData<float>(),
         in_step.getDim().height(), in_step.getDim().width(), epsilon);
+    } else if (in_step.getDataType() == ml::train::TensorDim::DataType::FP16) {
+#ifdef ENABLE_FP16
+      nntrainer::rms_norm_wrt_width_fp16_intrinsic(
+        in_step.getData<_FP16>(), out_step.getData<_FP16>(),
+        in_step.getDim().height(), in_step.getDim().width(), epsilon);
+#else
+      throw std::invalid_argument("Error: enable-fp16 is not set");
 #endif
     } else {
       throw std::invalid_argument(
