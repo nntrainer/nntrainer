fc8dd5b7c5b80946e2a64c1193c073607a01a13e
Pranjal Thapliyal <p.thapliyal@samsung.com>
2026-06-29 17:41:48 +0530

    [CausalLM/layers] implement RMSNorm backward pass for LoRA training
    Enable gradient computation through the Root Mean Square normalisation
    layer. Adds an inv_rms intermediate tensor (ITERATION_LIFESPAN) cached
    during the forward pass and used in calcGradient() to compute exact
    gradients with respect to both the normalised input and the gamma scale
    weight. Also overrides supportBackwarding() to return true.
    
    Co-authored-by: Niket Agarwal <niket.a@samsung.com>
    Signed-off-by: Pranjal Thapliyal <p.thapliyal@samsung.com>
    Signed-off-by: Niket Agarwal <niket.a@samsung.com>

--- Applications/CausalLM/layers/rms_norm.h
+++ Applications/CausalLM/layers/rms_norm.h
@@ -7,6 +7,8 @@
  * @brief  Implementation of RMS normalization function
  * @see    https://github.com/nntrainer/nntrainer
  * @author Seungbaek Hong <sb92.hong@samsung.com>
+ * @author Pranjal Thapliyal <p.thapliyal@samsung.com>
+ * @author Niket Agarwal <niket.a@samsung.com>
  * @bug    No known bugs except for NYI items
  * @note   This layer only supports inference mode.
  */
@@ -26,7 +28,7 @@
 #include <node_exporter.h>
 #include <utility>
 
-#include <causallm_common_properties.h>
+#include "causallm_common_properties.h"
 #include <connection.h>
 #include <tensor.h>
 #include <tensor_wrap_specs.h>
@@ -76,16 +78,21 @@ public:
   WIN_EXPORT void calcDerivative(nntrainer::RunLayerContext &context) override;
 
   /**
+   * @copydoc Layer::calcGradient(RunLayerContext &context)
+   */
+  WIN_EXPORT void calcGradient(nntrainer::RunLayerContext &context) override;
+
+  /**
    * @copydoc bool supportBackwarding() const
    */
-  WIN_EXPORT bool supportBackwarding() const override { return false; };
+  WIN_EXPORT bool supportBackwarding() const override { return true; };
 
   /**
    * @copydoc Layer::exportTo(Exporter &exporter, ExportMethods method)
    */
   WIN_EXPORT void
   exportTo(nntrainer::Exporter &exporter,
-           const ml::train::ExportMethods &method) const override{};
+           const ml::train::ExportMethods &method) const override {};
 
   /**
    * @copydoc Layer::getType()
@@ -111,7 +118,7 @@ public:
   inline static const std::string type = "rms_norm";
 
 private:
-  std::array<unsigned int, 1> wt_idx;
+  std::array<unsigned int, 3> wt_idx;
   std::tuple<props::RMS_NORM_GAMMA_INIT, nntrainer::props::Epsilon,
              nntrainer::props::SkipPrefill>
     rms_props;
